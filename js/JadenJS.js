a3adcdd8e4918818099bde9ff89a079b6bf60cc5
blaze500 <57372238+blaze500@users.noreply.github.com>
2021-10-31 06:12:14 -0400

    Add files via upload

--- js/JadenJS.js
+++ js/JadenJS.js
@@ -1,12 +1,12 @@
-if(window.innerHeight<window.innerWidth){
+if(window.innerHeight<=window.innerWidth){
 	document.getElementById("pageSizeBox").style.height= window.innerHeight*3.5;
-	document.getElementById("pageContentBox").style.width= 75%;
-	document.getElementById("CommentSubmissionForm").style.height= 70%;
-	document.getElementById("SubmitButton").style.height=10%;
+	document.getElementById("pageContentBox").style.width= '75%';
+	document.getElementById("CommentSubmissionForm").style.height= '70%';
+	document.getElementById("SubmitButton").style.height= '10%';
 }
 else{
 	document.getElementById("pageSizeBox").style.height= window.innerHeight*1.5;
-	document.getElementById("pageContentBox").style.width= 100%;
-	document.getElementById("CommentSubmissionForm").style.height= 80%;
-	document.getElementById("SubmitButton").style.height=20%;	
+	document.getElementById("pageContentBox").style.width= '100%';
+	document.getElementById("CommentSubmissionForm").style.height= '80%';
+	document.getElementById("SubmitButton").style.height='20%';	
 }
